2fcb1d65c6adb4b70c53960a23c6bdf435f42023
yaman0 <yaman0@protonmail.com>
2019-02-06 17:15:58 +0100

    feat(): add euclide extend function

--- src/Tools.py
+++ src/Tools.py
@@ -5,3 +5,22 @@ def modpow(n, pow, mod):
         return (modpow(n, pow / 2, mod) * modpow(n, pow / 2, mod)) % mod
     else:
         return (n * modpow(n, pow / 2, mod) * modpow(n, pow / 2, mod)) % mod
+
+
+def euclideDiv(a, b):
+    r = a % b
+    q = (a - r) / b
+    return q, r
+
+
+def euclidExtend(a, b):
+    r = 1
+    u, v = 1, 0
+    u_, v_ = 0, 1
+    _u, _v, _r = 0, 0, 0  # Temp vars
+    while b != 0:
+        q, r = euclideDiv(a, b)  # divide
+        _r, _u, _v = a, u, v  # update temp
+        a, u, v = b, u_, v_  # shift
+        b, u_, v_ = r, _u - q * u_, _v - q * v_  # computed
+    return a, u, v
--- test/test_Tools.py
+++ test/test_Tools.py
@@ -3,3 +3,15 @@ from src.Tools import *
 
 def test_modpow_should_works():
     assert modpow(4, 13, 497) == 445
+
+def test_euclideDiv_should_works():
+    quotient, rest = euclideDiv(13, 6)
+    assert quotient == 2
+    assert rest == 1
+
+def test_euclidExtend_should_works():
+    pgcd, u, v = euclidExtend(135, 101)
+    assert pgcd == 1
+    assert u == 3
+    assert v == -4
+
