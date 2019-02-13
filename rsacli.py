0f3cefcbf3771a39b299db938e597efbe41daf60
yaman0 <yaman0@protonmail.com>
2019-02-13 22:56:20 +0100

    feat(cli): add methods to stn and nts

--- rsacli.py
+++ rsacli.py
@@ -21,10 +21,16 @@ def decode(args):
     message = RSA.dechiffrement(message, args.n, d)
     print StringTool.intToString(message)
 
+def stringtonumber(args):
+    print StringTool.stringToInt(args.string)
+
+def numbertostring(args):
+    print StringTool.stringToInt(args.number)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # Generation of pi parse
+
     subparser = parser.add_subparsers(dest='cmd')
     subparser.required = True
     crypt_parse = subparser.add_parser('crypt', help='Crypt message')
@@ -37,7 +43,6 @@ if __name__ == '__main__':
     crypt_parse.set_defaults(func=crypt)
     crypt_parse = subparser.add_parser('crypt', help='Crypt message')
 
-    #
     decode_parse = subparser.add_parser('decode', help='Crypt message')
     decode_parse.add_argument("message", help="message to decode",
                               type=str)
@@ -47,7 +52,6 @@ if __name__ == '__main__':
                               type=int)
     decode_parse.set_defaults(func=decode)
 
-    #
     decrypt_parse = subparser.add_parser('decrypt', help='Crypt message')
     decrypt_parse.add_argument("message", help="message to decrypt",
                                type=str)
@@ -56,6 +60,16 @@ if __name__ == '__main__':
     decrypt_parse.add_argument("d", help="d from public key",
                                type=int)
     decrypt_parse.set_defaults(func=decrypt)
+
+    stn_parse = subparser.add_parser('stn', help='String to number')
+    stn_parse.add_argument("string", help="String to number",
+                               type=str)
+    stn_parse.set_defaults(func=stringtonumber)
+
+    nts_parse = subparser.add_parser('nts', help='Number to string')
+    nts_parse.add_argument("number", help="Number to string",
+                           type=int)
+    nts_parse.set_defaults(func=numbertostring)
 
     args = parser.parse_args()
     args.func(args)
